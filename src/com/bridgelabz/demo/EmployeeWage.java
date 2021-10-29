87a353e272d142fc9c1ca1c8ad552c6eeb18b96e
priyankap <priyanka123360@gmail.com>
2021-10-29 16:35:50 +0530

    Added : Employee Wage is calculation logic

--- src/com/bridgelabz/demo/EmployeeWage.java
+++ src/com/bridgelabz/demo/EmployeeWage.java
@@ -4,11 +4,18 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage");
 		int present = 1;
+		
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == present) {
-			System.out.println("Employee is present");
+			int emp_rate_per_hrs = 20;
+			int emp_hrs = 8;
+			int emp_wage = 0;
+			emp_wage = emp_rate_per_hrs * emp_hrs; 
+			System.out.println("Employee Wage :" + emp_wage );
+			
 		} else
-			System.out.println("Employee is Absent");
+			
+			System.out.println("Employee Wage is zero");
 
 	}
 
