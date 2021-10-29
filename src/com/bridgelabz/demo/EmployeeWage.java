9b4e25d0875ba5ac8597a3d19f48e817fb0a7dfe
priyankap <priyanka123360@gmail.com>
2021-10-29 18:35:33 +0530

    Added : Employee Wage using Switch case logic

--- src/com/bridgelabz/demo/EmployeeWage.java
+++ src/com/bridgelabz/demo/EmployeeWage.java
@@ -1,24 +1,24 @@
 package com.bridgelabz.demo;
 
 public class EmployeeWage {
+	public static final int full_time = 1;
+	public static final int part_time = 2;
+	public static final int emp_rate_per_hrs = 20;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage");
-		int full_time = 1;
-		int part_time = 2;
-		int emp_rate_per_hrs = 20;
 		int emp_wage = 0;
 		int emp_hrs = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == full_time) 
-			emp_hrs = 8;
-					
-		else 
-			if (empCheck == part_time) 
-				emp_hrs = 4;
-				
-			else 
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck ) {
+		case full_time:
+		    emp_hrs = 8;
+			break;		
+		case  part_time :
+			emp_hrs = 4;
+			break;
+			default:
 				 emp_hrs = 0;
-			
+		}
 		
 	    	emp_wage = emp_hrs * emp_rate_per_hrs ; 
 			System.out.println("Employee Wage :" + emp_wage );
