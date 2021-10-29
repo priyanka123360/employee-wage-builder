bb92b21adae49799b113d48166006d523309e10b
priyankap <priyanka123360@gmail.com>
2021-10-29 09:45:11 +0530

    Added : employee present or absent logic

--- src/com/bridgelabz/demo/EmployeeWage.java
+++ src/com/bridgelabz/demo/EmployeeWage.java
@@ -3,7 +3,13 @@ package com.bridgelabz.demo;
 public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage");
-		
+		int present = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == present) {
+			System.out.println("Employee is present");
+		} else
+			System.out.println("Employee is Absent");
+
 	}
 
 }
