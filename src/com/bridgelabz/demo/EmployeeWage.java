5162f29aee975e6cdd067742296d1fb2d7e6f872
priyankap <priyanka123360@gmail.com>
2021-10-29 17:40:03 +0530

    Added : Employee Part Time logic

--- src/com/bridgelabz/demo/EmployeeWage.java
+++ src/com/bridgelabz/demo/EmployeeWage.java
@@ -3,19 +3,26 @@ package com.bridgelabz.demo;
 public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage");
-		int present = 1;
+		int full_time = 1;
+		int part_time = 2;
+		int emp_rate_per_hrs = 20;
+		int emp_wage = 0;
+		int emp_hrs = 0;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == full_time) 
+			emp_hrs = 8;
+					
+		else 
+			if (empCheck == part_time) 
+				emp_hrs = 4;
+				
+			else 
+				 emp_hrs = 0;
+			
 		
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == present) {
-			int emp_rate_per_hrs = 20;
-			int emp_hrs = 8;
-			int emp_wage = 0;
-			emp_wage = emp_rate_per_hrs * emp_hrs; 
+	    	emp_wage = emp_hrs * emp_rate_per_hrs ; 
 			System.out.println("Employee Wage :" + emp_wage );
 			
-		} else
-			
-			System.out.println("Employee Wage is zero");
 
 	}
 
