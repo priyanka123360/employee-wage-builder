eab5ca11584445533167c4fc43862baa526e93ea
priyankap <priyanka123360@gmail.com>
2021-10-30 15:27:55 +0530

    Added : Employee Wage for 20 days of Month logic

--- src/com/bridgelabz/demo/EmployeeWage.java
+++ src/com/bridgelabz/demo/EmployeeWage.java
@@ -4,14 +4,19 @@ public class EmployeeWage {
 	public static final int full_time = 1;
 	public static final int part_time = 2;
 	public static final int emp_rate_per_hrs = 20;
-	public static final int num_of_working_days = 2;
+	public static final int num_of_working_days = 20;
+	public static final int hrs_in_month = 100;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage");
-		int emp_wage = 0;
+		
 		int emp_hrs = 0;
-		int total_emp_wage = 0;
-		for (int day = 0; day < num_of_working_days; day++) {
+		int total_emp_hrs = 0;
+		int total_working_days = 0;
+		int emp_wage = 0;
+		while (total_emp_hrs <= hrs_in_month && total_working_days <  num_of_working_days ) {
+			total_working_days++;
+			
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case full_time:
@@ -24,10 +29,10 @@ public class EmployeeWage {
 				emp_hrs = 0;
 			}
 
-			emp_wage = emp_hrs * emp_rate_per_hrs;
-			total_emp_wage = total_emp_wage + emp_wage;
-			System.out.println("Employee Wage :" + emp_wage);
+			total_emp_hrs = total_emp_hrs + emp_wage;
+			System.out.println("Day# :" +total_working_days + "Emp Hr :" + emp_wage );
 		}
-		System.out.println("Total Emp Wage :" + total_emp_wage);
+		emp_wage = emp_hrs * emp_rate_per_hrs;
+		System.out.println("Total Emp Wage :" + emp_wage);
 	}
 }
