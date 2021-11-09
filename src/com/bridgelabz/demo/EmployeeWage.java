e7ffacd89898f5f1aa60f1c7b6383b19431e6e80
priyankap <priyanka123360@gmail.com>
2021-11-09 16:49:34 +0530

    Added : Use Class Method to Compute Employee Wage.

--- src/com/bridgelabz/demo/EmployeeWage.java
+++ src/com/bridgelabz/demo/EmployeeWage.java
@@ -7,16 +7,15 @@ public class EmployeeWage {
 	public static final int num_of_working_days = 20;
 	public static final int hrs_in_month = 100;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage");
-		
+	public static int computeEmpWage() {
+
 		int emp_hrs = 0;
 		int total_emp_hrs = 0;
 		int total_working_days = 0;
 		int emp_wage = 0;
-		while (total_emp_hrs <= hrs_in_month && total_working_days <  num_of_working_days ) {
+		while (total_emp_hrs <= hrs_in_month && total_working_days < num_of_working_days) {
 			total_working_days++;
-			
+
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case full_time:
@@ -30,9 +29,15 @@ public class EmployeeWage {
 			}
 
 			total_emp_hrs = total_emp_hrs + emp_wage;
-			System.out.println("Day# :" +total_working_days + "Emp Hr :" + emp_wage );
+			System.out.println("Day# :" + total_working_days + "Emp Hr :" + emp_wage);
 		}
 		emp_wage = emp_hrs * emp_rate_per_hrs;
 		System.out.println("Total Emp Wage :" + emp_wage);
+		return emp_wage;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage");
+		EmployeeWage.computeEmpWage();
 	}
 }
