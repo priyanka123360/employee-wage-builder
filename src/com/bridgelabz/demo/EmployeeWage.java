59b819ede7c6be5f7f4b4037861d084ee660304f
priyankap <priyanka123360@gmail.com>
2021-11-20 21:57:40 +0530

    Modified : total Wage of company logic

--- src/com/bridgelabz/demo/EmployeeWage.java
+++ src/com/bridgelabz/demo/EmployeeWage.java
@@ -2,6 +2,8 @@ package com.bridgelabz.demo;
 
 import java.util.ArrayList;
 
+import java.util.Scanner;
+
 public class EmployeeWage implements EmployeeService {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
@@ -27,12 +29,24 @@ public class EmployeeWage implements EmployeeService {
 		}
 	}
 
-	
+	@Override
+	public int getTotalWage(String company) {
+		int totalWage = 0;
+
+		for (int i = 0; i < companyEmpWageList.size(); i++) {
+			if (company.equals(companyEmpWageList.get(i).getCompany())) {
+				totalWage = companyEmpWageList.get(i).getTotalEmpWage();
+				break;
+			}
+		}
+		return totalWage;
+	}
+
 
 	private int computeEmpWage(CompanyEmployeeWage companyEmpWage) {
-		
+		// variables
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-		
+		// Computation
 		while (totalEmpHrs <= companyEmpWage.getMaxHoursPerMonth()
 				&& totalWorkingDays < companyEmpWage.getNumOfWorkingDays()) {
 			totalWorkingDays++;
@@ -59,6 +73,16 @@ public class EmployeeWage implements EmployeeService {
 		employeeWage.addCompanyEmpWage("BigBazar", 30, 50, 100);
 		employeeWage.addCompanyEmpWage("ShreeMall", 40, 30, 120);
 		employeeWage.computeEmpWage();
+		System.out.println("Enter the Company name");
+		Scanner sc=new Scanner(System.in);
+		String company=sc.next();
+		int totalCompanyWage=employeeWage.getTotalWage(company);
+		if(totalCompanyWage>0) {
+		System.out.print(company +" = " +totalCompanyWage);
+		}
+		else
+			System.out.print("There is no company name with "+company);
+		
 	}
+
 }
-	
